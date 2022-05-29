d59a092d1e27926e96100e20128365c2eaf322d1
3185281991 <82033448+3185281991@users.noreply.github.com>
2022-05-29 08:47:12 -0500

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -47,11 +47,22 @@ const router = new VueRouter({
 });
 //全局导航守卫更改标题显示
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title;
   //每次进入路由后，需要保存这个路由信息，以便再刷新的时候跳转到这个路由
   sessionStorage.setItem("active", to.path);
-  //当路径变为login时，不能返回，清空数据
-  return next();
+  document.title = to.meta.title;
+  //每次判断token是否存在
+  let token = localStorage.getItem("token") || ""; //在本地存储中获取token
+  if (token) {
+    //判断是否有token
+    next();
+  } else {
+    //在没有token的前提下，to下面的path是否为/login，如果不是则页面跳转到登录页面
+    if (to.path == "/login") {
+      next();
+    } else {
+      next({ path: "/login" }); //跳转页面到login页
+    }
+  }
 });
 
 export default router;
