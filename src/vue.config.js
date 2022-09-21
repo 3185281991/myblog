ca4c397ec301ad5b997c87da90ccc77437e70b1b
3185281991 <82033448+3185281991@users.noreply.github.com>
2022-09-21 10:53:35 +0800

    Update vue.config.js

--- src/vue.config.js
+++ src/vue.config.js
@@ -20,19 +20,6 @@ module.exports = {
 
       .set("network", resolve("./src/network"));
   },
-  css: {
-    loaderOptions: {
-      css: {},
-      postcss: {
-        plugins: [
-          require("postcss-px2rem")({
-            remUnit: 192, //根据设计图
-            //375的图就给37.5，也就是1rem=37.5px
-          }),
-        ],
-      },
-    },
-  },
   configureWebpack: {
     //这里是添加的部分
     externals: {
